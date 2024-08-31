a8d22f020ea5ce5456f787afdb079e5aab818dc3
mouradjwz <70758569+mouradjwz@users.noreply.github.com>
2024-08-31 20:31:32 +0200

    Add files via upload

---
+++ Dockerfile-Project/src/main/java/CCR/Dockerfile/Project/DockerfileProjectApplication.java
@@ -0,0 +1,13 @@
+package CCR.Dockerfile.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerfileProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerfileProjectApplication.class, args);
+	}
+
+}
